0cadfd39b440fcfe039fd81e79237c9bab8b2bf8
ssingle2002@gmail.com <ssingle2002@gmail.com>
2018-06-05 03:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,10 @@ const two = 2;
 var funkyFunction = function return {function() {return "FUNKY!"}}
 
 
-var theFunk = function funkyFunction()
+
 
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-var theFunk = funkyFunction();
-
+var theFunk = function funkyFunction()
